656a14057aebabdcab971c9016554ca99fb041dd
Chris <chrisbaird05@gmail.com>
2019-11-15 12:22:40 -0800

    progress

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 import AddStudentForm from '../addStudentForm/AddStudentForm';
 import StudentCard from '../StudentCard/StudentCard';
 import axios from 'axios';
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       students: [],
-      user: {}
+      user: {},
+      modal: false
     };
     this.handleAddStudent = this.handleAddStudent.bind(this);
+    this.handleModalToggle = this.handleModalToggle.bind(this);
   }
 
   handleAddStudent(newStudent) {
     const students = [...this.state.students, newStudent];
     this.setState({ students: students });
+  }
+
+  handleModalToggle() {
+    this.setState({ modal: !this.state.modal });
   }
 
   componentDidMount() {
@@ -32,9 +38,32 @@ class Dashboard extends Component {
       <div>
         <h1>Dashboard</h1>
         <hr />
-        <button className="btn btn-info m-1">Add Student</button>
+        <button className="btn btn-info m-1" onClick={this.handleModalToggle}>
+          Add Student
+        </button>
         <button className="btn btn-info m-1">Create Email</button>
         <button className="btn btn-info m-1">Send Weekly Email</button>
+        <Modal
+          isOpen={this.state.modal}
+          centered={true}
+          toggle={this.handleModalToggle}
+        >
+          <ModalHeader toggle={this.handleModalToggle}>Add Student</ModalHeader>
+          <ModalBody>
+            <AddStudentForm
+              user={this.props.user}
+              addStudent={this.handleAddStudent}
+            />
+          </ModalBody>
+          <ModalFooter>
+            {/* <Button color="primary" onClick={toggle}>
+              Do Something
+            </Button>{' '}
+            <Button color="secondary" onClick={toggle}>
+              Cancel
+            </Button> */}
+          </ModalFooter>
+        </Modal>
         {/* <AddStudentForm
           user={this.props.user}
           addStudent={this.handleAddStudent}
